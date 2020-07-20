073bd1f2dcb74d49bba55cf8541f0c458f8a2b0a
alexandru <alexandruserban160@gmail.com>
2020-07-20 21:48:20 +0300

    initial commit of my first 3 scripts ( dire roller, guess the number and rock paper scissors)

---
+++ dice_roller.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+def dice_roller():
+    dice1 = randint(1, 6)
+    dice2 = randint(1, 6)
+    print("You'r roll was " + str(dice1) + " and " + str(dice2))
+    retry_question = input("Do you wish to try again? ( y/n ) ")
+    if retry_question[0] == "y" or retry_question[0] == "Y":
+        dice_roller()
+
+    else:
+        print("Thanks for using my small script it means a lot to me ^^")
+
+
+dice_roller()
---
+++ rock_paper_scissors.py
@@ -0,0 +1,35 @@
+from random import randint
+
+
+def rock_paper_scissors():
+    choices = ['rock', 'paper', 'scissors']
+    script_choice = str(choices[randint(0, 2)])
+    user_choice = str(input(
+        "Chose one of three : rock(r), paper(p), scissors(s) ( CASE SENSITIVE ) "))
+    # Tie resolution
+    if user_choice[0] == script_choice[0]:
+        print("Is's a tie!")
+    # User rock choice resolution
+    if user_choice[0] == "r" and script_choice[0] == "p":
+        print("Paper beats rock you've lost")
+    elif user_choice[0] == "r" and script_choice[0] == "s":
+        print("Rock beats scissors you've won")
+    # User scissors choice resolution
+    if user_choice[0] == "s" and script_choice[0] == "r":
+        print("Rock beats scissors you've lost")
+    elif user_choice[0] == "s" and script_choice[0] == "p":
+        print("Scissors beats paper you've won")
+    # User paper choice resolution
+    if user_choice[0] == "p" and script_choice[0] == "r":
+        print("Paper beats rock you've won")
+    elif user_choice[0] == "p" and script_choice[0] == "s":
+        print("Scissors beat paper you've lost")
+    # Replay
+    replay = input("Wanna play again ( y/n ) ? ")
+    if replay[0] == "y" or replay[0] == "Y":
+        rock_paper_scissors()
+    else:
+        print("Thanks for playing")
+
+
+rock_paper_scissors()
