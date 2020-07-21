4a1d4ccae1434c8d60cdbbd6a7a91e2129bab660
alexandru <alexandruserban160@gmail.com>
2020-07-21 11:35:34 +0300

    removed an extra condition for an upper case Y

--- dice_roller.py
+++ dice_roller.py
@@ -5,8 +5,8 @@ def dice_roller():
     dice1 = randint(1, 6)
     dice2 = randint(1, 6)
     print("You'r roll was " + str(dice1) + " and " + str(dice2))
-    retry_question = input("Do you wish to try again? ( y/n ) ")
-    if retry_question[0] == "y" or retry_question[0] == "Y":
+    retry_question = input("Do you wish to try again? ( y/n ) ").lower()
+    if retry_question[0] == "y":
         dice_roller()
 
     else:
