d6fd02fb18dd681552a3972a23b7330ba42a42f6
alexandru <alexandruserban160@gmail.com>
2020-07-21 21:05:55 +0300

    a very basic password generator

---
+++ password_generatore.py
@@ -0,0 +1,22 @@
+import string
+from random import choice
+
+
+def password_generatore():
+    characters = string.ascii_letters + string.punctuation + string.digits
+    try:
+        password_range = int(
+            input("How long do you wish you password to be ( 8-16 recomandet )? "))
+        password = "".join(choice(characters) for x in range(password_range))
+        print(password)
+        replay = input("Do you wish to try again? y/n ").lower()
+        if replay[0] == "y":
+            password_generatore()
+        else:
+            print("Thanks for using my small script it means a lot to me ^^")
+    except ValueError:
+        print("Please enter a number not a letter or punctuation try again. ")
+        password_generatore()
+
+
+password_generatore()
