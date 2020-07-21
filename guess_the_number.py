4c1d0b7275011113988fccb8152672042d46d777
alexandru <alexandruserban160@gmail.com>
2020-07-21 21:01:55 +0300

    added a try and except functionality to rezolve an issue with entering letters or punctuation on input variables

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,18 +2,22 @@ from random import randint
 
 
 def guess_the_number():
-    user_range = int(input(
-        "What is the range from which a random number will be chosen ? "))
-    number_chosen = randint(1, int(user_range))
-    user_guess = input("Guess the number? ")
-    while not number_chosen == int(user_guess):
-        user_guess = int(input("Wrong answer try again "))
-        if user_guess == number_chosen:
-            print("Well done you've guessed the number!!")
-    replay = input("Do you wish to play again? ( y/n ) ").lower()
-    if replay[0] == "y":
+    try:
+        user_range = int(input(
+            "What is the range from which a random number will be chosen ? "))
+        number_chosen = randint(1, int(user_range))
+        user_guess = input("Guess the number? ")
+        while not number_chosen == int(user_guess):
+            user_guess = int(input("Wrong answer try again "))
+            if user_guess == number_chosen:
+                print("Well done you've guessed the number!!")
+        replay = input("Do you wish to play again? ( y/n ) ").lower()
+        if replay[0] == "y":
+            guess_the_number()
+        else:
+            print("Thanks for using my script it means a lot to me ^^")
+    except ValueError:
+        print("Please enter a number not a letter or punctuation try again. ")
         guess_the_number()
-    else:
-        print("Thanks for using my script it means a lot to me ^^")
 
 guess_the_number()
