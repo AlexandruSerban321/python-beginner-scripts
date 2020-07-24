4e434a4ed703588e2f08d39e24016b9ad6587b8e
alexandru <alexandruserban160@gmail.com>
2020-07-24 16:39:16 +0300

    added a loop for inproper answers on retry question

--- dice_roller.py
+++ dice_roller.py
@@ -6,10 +6,11 @@ def dice_roller():
     dice2 = randint(1, 6)
     print("You'r roll was " + str(dice1) + " and " + str(dice2))
     retry_question = input("Do you wish to try again? ( yes/no ) ").lower()
-    if retry_question == "yes":
-        dice_roller()
-    elif retry_question == 'no':
-        print("Thanks for using my small script it means a lot to me ^^")
-
+    while not retry_question == 'yes' or retry_question == 'no':
+        retry_question = input("Plase answer with 'yes' or 'no': ")
+        if retry_question == "yes":
+            dice_roller()
+        elif retry_question == 'no':
+            print("Thanks for using my small script it means a lot to me ^^")
 
 dice_roller()
