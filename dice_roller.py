62fbe102a695c999201c60c41f99f1a291ece841
alexandru <alexandruserban160@gmail.com>
2020-07-24 16:36:15 +0300

    changed parameters of the if function to 'yes' and 'no' from 'y' and 'n'

--- dice_roller.py
+++ dice_roller.py
@@ -5,11 +5,10 @@ def dice_roller():
     dice1 = randint(1, 6)
     dice2 = randint(1, 6)
     print("You'r roll was " + str(dice1) + " and " + str(dice2))
-    retry_question = input("Do you wish to try again? ( y/n ) ").lower()
-    if retry_question[0] == "y":
+    retry_question = input("Do you wish to try again? ( yes/no ) ").lower()
+    if retry_question == "yes":
         dice_roller()
-
-    else:
+    elif retry_question == 'no':
         print("Thanks for using my small script it means a lot to me ^^")
 
 
